c46652db9a0a3b6a27f7b84ff9b2abfc1b774f03
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2021-09-29 14:58:06 +0100

    mostly complete figure updates

--- Makefile
+++ Makefile
@@ -5,7 +5,10 @@ main.pdf: WOR.tex refs.bib auths.tex main.png WellcomeOR_styles.sty WellcomeOR_l
 	pdflatex $<
 	pdflatex $<
 
-R = Rscript $^ $@
+R = $(strip Rscript $^ $(1) $@)
+
+REFSPEC ?= 0.99
+REFCOV  ?= 0.2
 
 main.png: fig_main.R
-	${R}
+	$(call R,${REFSPEC} ${REFCOV})
--- fig_main.R
+++ fig_main.R
@@ -2,127 +2,123 @@
 suppressPackageStartupMessages({
   require(data.table)
   require(ggplot2)
-  require(metR)
+  require(ggrepel)
   require(patchwork)
 })
 
+#' want two panels, which provide two complementary "maps" of how to think
+#' about introducing an Ab-testing scheme
+#'  - how much would that scheme expand some baseline coverage level?
+#'  - when does it make more sense to buy additional coverage with testing vs vaccination?
+#'  
+#' for these "maps", the reader would work from measurable values, navigate the "maps"
+#' and find the answer to these questions
+#' 
+#' we're choosing the following the navigation scheme:
+#'  - scan across to likely seroprevalence in a population
+#'  - go up (perpendicular to x-axis) to test sensitivity contour line
+#'  - go left (parallel to x-axis) to look up value on y-axis
+#'  
+#' As discussed in the manuscript, we present "maps" for
+#' particular specificity & reference baseline coverage assumptions,
+#' since the maps are to illustrate the result (i.e. the relationship equations)
+#' not themselves the result
+#' 
+#' if you want different "maps", use different arguments when invoking this
+#' script or change the defaults below / in the Makefile. Also, we choose
+#' particular questions, but others are answerable with the relations after
+#' a bit of algebra (e.g., given a test price, what is required sensitivity
+#' for it to be a preferable purchase)
+
+.defaults <- c(specificity = 0.99, coverage = 0.20)
 .args <- if (interactive()) c(
-  "main.png"
+  .defaults, "main.png"
 ) else commandArgs(trailingOnly = TRUE)
+
+refspec <- as.numeric(.args[1])
+covaxcoverage <- as.numeric(.args[2])
 
 PPDmul <- function(TNR, TPR, seropos) TNR + seropos*(1+TPR-TNR)
 
-#' if we think about contour lines correspond to PPDmul == 1,
-#' these are the (exclusive) minimum TPRs required to achieve
-#' any expansion in coverage
-#' TPR = (1 - TNR)*(1/seropos - 1)
-#' @examples
-#' TPRcontour <- function(TNR, seropos) (1 - TNR)*(1/seropos - 1)
-#' surface.dt <- data.table(expand.grid(seropos = seq(0,1,by=0.01), TNR = seq(0,1,by=0.01)))
-#' surface.dt[, TPR := TPRcontour(TNR, seropos)]
-#' ggplot(surface.dt[TPR <= 1]) + geom_contour_filled(aes(seropos, TNR, z=TPR))
+targetTPRcontours = c(seq(1,9,by=2)/10, 0.95, 0.99)
 
-#' alternatively, if we consider some baseline coverage & some fixed (high) TNR
-#' how does TPR expand that coverage?
-#' newcoverage = basecoverage*(TNR + seropos*(1+TPR-TNR)) =>
-#' (newcoverage/basecoverage - TNR)/seropos - (1-TNR) = TPR
+#' relationship is linear in seropositive fraction,
+#' so can just get line endpoints
+seroposscale <- c(0.2, 0.8)
 
-covaxcoverage <- .2
-refspec <- .99
-TPRcovcontour <- function(
-  seropos, expcoverage, basecoverage = covaxcoverage, TNR = refspec
-) ((expcoverage/basecoverage) - TNR)/seropos - (1-TNR)
-
-seroposscale <- seq(0.2,0.8,by=0.0001)
-
-ref <- data.table(expand.grid(
-  seropos = seroposscale,
-  expcoverage = seq(0, .5,by=0.0001)
-))
-ref[, TPR := TPRcovcontour(seropos, expcoverage) ]
-
-TPRres <- function(x) fifelse(x < 0.5, (x/0.5)*0.2, 0.2+(x-0.5)/0.5*0.8)
-inv.rescale <- function(x) fifelse(x < 0.2, x/0.2*0.5, 0.5+(x-0.2)/0.8*0.5)
-
-ref[, TPRrescale := TPRres(TPR) ]
-
-TPRlines <- data.table(expand.grid(
-  seropos = seq(0.2, 0)
+refCoverageMap <- data.table(expand.grid(
+  TPR = targetTPRcontours,
+  seropos = seroposscale
 ))
 
-plottercore <- function(
-  dt, aesydef
-) ggplot(dt[between(TPR,0,1)]) +
-  aes(x = seropos, fill = TPRrescale) +
-  aesydef + geom_raster() +
-  geom_contour(
-    aes(z=TPR),
-    breaks = c(0.1, 0.3, 0.5, 0.7, 0.9, 0.95, 0.99),
-    show.legend = FALSE,
-    color = "black"
+refCoverageMap[, TVratio := PPDmul(refspec, TPR, seropos)-1 ]
+refCoverageMap[, coverage := covaxcoverage*(TVratio+1) ]
+
+plottercore <- function(dt, aesydef, yl) ggplot(dt) +
+  aes(x = seropos, color = "black") +
+  aesydef + 
+  geom_line(aes(group=TPR)) +
+  geom_text_repel(
+    aes(label = scales::percent(TPR, accuracy = 1)),
+    data = function(ldt) ldt[seropos == max(seropos)],
+    direction = "y",
+    hjust = "left",
+    size = 2,
+    nudge_x = 0.01,
+    box.padding = 0.01,
+    xlim = c(NA, Inf),
+    show.legend = FALSE
   ) +
-  geom_text_contour(
-    aes(z=TPR, label = sprintf("%i%%", round(stat(level)*100))),
-    color = "white", stroke.color = "black",
-    breaks = c(0.1, 0.3, 0.5, 0.7, 0.9, 0.95, 0.99),
-    skip = 0,
-    show.legend = FALSE,
-    stroke = 0.2, check_overlap = TRUE
-  ) + 
+  coord_cartesian(
+    ylim = yl, xlim = range(seroposscale),
+    clip = "off"
+  ) +
   scale_x_continuous(
     "% Seropositive",
     minor_breaks = NULL,
     labels = scales::percent_format(accuracy = 1)
-  ) +
-  scale_fill_continuous(
-    "Test Sensitivity",
-    breaks = TPRres(c(0.1, 0.3, 0.5, 0.7, 0.9, 0.95, 0.99)),
-    labels = function(x) scales::percent(inv.rescale(x), accuracy = 1)
+  ) + scale_color_manual(
+    name = NULL,
+    values = c(black = "black"),
+    labels = sprintf("Test sensitivity, X%%\nspecificity %i%%", round(refspec*100))
   )
 
-pA <- plottercore(ref, aes(y=expcoverage)) +
-  coord_cartesian(
-    xlim=c(0.2, 0.8),
-    ylim=c(0, covaxcoverage*2),
-    expand = FALSE
-  ) +
+pA <- function(
+  dt,
+  yl.cov=c(0, covaxcoverage*2),
+  ylres.cov=diff(yl.cov)/4,
+  ...
+) plottercore(dt, aes(y=coverage), yl.cov) +
   scale_y_continuous(
-    "Total Immunized %, Assuming COVAX-like Baseline Coverage",
-    breaks = seq(0.2, 0.4, by=0.05),
+    "Total Immunized %\nAssuming COVAX-like Baseline Coverage",
+    breaks = seq(yl.cov[1], yl.cov[2], by=ylres.cov),
     minor_breaks = NULL,
     labels = scales::percent_format(accuracy = 1)
   )
 
-#' (T/V + (1-TNR))/seropos - (1-TNR) <= TPR  
-
-refB <- data.table(expand.grid(
-  seropos = seroposscale,
-  TVratio = seq(0, 1, by=0.001)
-))
-
-refB[, TPR := (TVratio + (1-refspec))/seropos - (1-refspec) ]
-refB[, TPRrescale := TPRres(TPR) ]
-
-pB <- plottercore(refB, aes(y=TVratio)) +
-  coord_cartesian(
-    xlim=c(0.2, 0.8),
-    ylim=c(0, 1),
-    expand = FALSE
-  ) +
+pB <- function(
+  dt,
+  yl.cost = c(0, 1),
+  ylres.cost = diff(yl.cost)/4,
+  ...
+) plottercore(dt[TPR != 0.01], aes(y=TVratio), yl.cost) +
   scale_y_continuous(
-    "Testing Cost % of Vaccination Dose Cost",
-    breaks = seq(0, 1, by=0.25),
+    "Testing Cost % of\nVaccination Dose Cost",
+    breaks = seq(yl.cost[1], yl.cost[2], by=ylres.cost),
     minor_breaks = NULL,
     labels = scales::percent_format(accuracy = 1)
   )
 
-res.p <- pA / pB &
-  theme_minimal() &
-  plot_layout(guides = "collect") &
-  plot_annotation(tag_levels = "A")
+fullplotter <- function(...) {
+  upper <- pA(...)
+  lower <- pB(...)
+  (upper / lower + plot_layout(guides = "collect") + plot_annotation(tag_levels = "A")) &
+  theme_minimal(base_size = 10) & theme(
+    plot.margin = margin(r = 1, unit = "line"),
+    legend.position = "bottom"
+  ) 
+}
 
-# one color bar fine
-# embiggen labels
-# align contour labels - manually place at 65 (all except 95, 99 - put those at 70, 75)
+res.p <- fullplotter(dt = refCoverageMap)
 
-ggsave(tail(.args, 1), res.p, height = 15, width = 8, dpi = 600, bg = "white")
+ggsave(tail(.args, 1), res.p, height = 7.5, width = 4, dpi = 600, bg = "white")
